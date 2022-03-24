e5ac4d43d3f57e18acef829bc297b270de98eca0
Daniel Kim <dkk6651@rit.edu>
2022-03-24 05:01:06 -0400

    Finished BruteForce.java still working on DivideAndConquer.java

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -1,5 +1,15 @@
-public class BruteForce {
+public class BruteForce extends ClosestPairAlg{
     public Triple closestPair(Point[] P){
-
+        double lowest = Double.MAX_VALUE;
+        Triple triple = null;
+        for(int i = 0; i < P.length; i++){
+            for(int j = i+1; j < P.length; j++){
+                double dist = P[i].distance(P[j]);
+                if(dist < lowest){
+                    triple = new Triple(P[i], P[j], dist);
+                }
+            }
+        }
+        return triple;
     }
 }
--- src/DivideAndConquer.java
+++ src/DivideAndConquer.java
@@ -1,5 +1,30 @@
-public class DivideAndConquer {
-    public Triple closestPair(Point[] P){
+import java.util.*;
 
+public class DivideAndConquer extends ClosestPairAlg{
+    public Triple closestPair(Point[] P) {
+        int count = P.length;
+        if(count <= 3){
+            ClosestPairAlg a = new BruteForce();
+            return a.closestPair(P);
+        }
+
+        PriorityQueue<Point> xQueue = new PriorityQueue<>();
+        for(Point p : P){
+            Point point = new Pointx(p.x, p.y);
+            xQueue.add(point);
+        }
+
+        System.out.println(Arrays.toString(list));
+        double separation = list[count/2] + list[count/2 + 1];
+        Triple tripleLeft = closestPair((Point[]) Arrays.copyOfRange(xQueue.toArray(), 0, count/2));
+        Triple tripleRight = closestPair((Point[]) Arrays.copyOfRange(xQueue.toArray(), count/2 + 1, count));
+
+        double distance = Math.min(tripleLeft.dist, tripleRight.dist);
+
+        for(Point p : P){
+
+        }
+
+        return null;
     }
 }
